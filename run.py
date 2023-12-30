65b21cafb5df128def97ac15bceece944d4f67e6
Maya Thompson <mayathompson@Mayas-MacBook-Air.local>
2023-12-30 12:24:34 +0800

    add start_game function with a welcome message and input field for users name

--- run.py
+++ run.py
@@ -2,22 +2,22 @@ def game_board():
     """
     
     """
-
-    # Credit: Estherns / tictactoe game
-    board = ["", "", "",
-             "", "", "",
-             "", "", ""]
-    print(board[0] + " | " + board[1] + " | " + board[2] + "      " + "1|2|3")
-    print(board[3] + " | " + board[4] + " | " + board[5] + "      " + "4|5|6")
-    print(board[6] + " | " + board[7] + " | " + board[8] + "      " + "7|8|9")
+    board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print("---------")
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print("---------")
+    print(board[6] + " | " + board[7] + " | " + board[8])
 
 def start_game():
     """
-    
+    Prints a welcome message and asks for users name with an input
+
     """
-    print("Hello there Earthlings")
-    print("This is the fun game of Tic Tac Toe where you will fight to the death!")
-    print("Ok, maybe not to the death but until you're either a loser for life or eternal winner!")
+    print("")
+    print("Welcome to Tic Tac Toe!")
+    print("We first need to know who is challenging our fierce Computer.")
+    name = input("Please enter your name:\n")
     
 
 def players():
@@ -42,8 +42,9 @@ def player_positions():
 def check_winner():
     """
     
+    Asks user if they want to play again and is taken to another round with
+    the play_game function
     """
-
 
 
 def main():
@@ -52,3 +53,6 @@ def main():
     cleaner code
     """
     game_board()
+    start_game()
+
+main()
