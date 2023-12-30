09d1f6f696cf4f3048f8fc23cb6dd45e61c683c7
Maya Thompson <mayathompson@Mayas-MacBook-Air.local>
2023-12-30 11:46:57 +0800

    add initial function names and create game board

--- run.py
+++ run.py
@@ -1,3 +1,54 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def game_board():
+    """
+    
+    """
+
+    # Credit: Estherns / tictactoe game
+    board = ["", "", "",
+             "", "", "",
+             "", "", ""]
+    print(board[0] + " | " + board[1] + " | " + board[2] + "      " + "1|2|3")
+    print(board[3] + " | " + board[4] + " | " + board[5] + "      " + "4|5|6")
+    print(board[6] + " | " + board[7] + " | " + board[8] + "      " + "7|8|9")
+
+def start_game():
+    """
+    
+    """
+    print("Hello there Earthlings")
+    print("This is the fun game of Tic Tac Toe where you will fight to the death!")
+    print("Ok, maybe not to the death but until you're either a loser for life or eternal winner!")
+    
+
+def players():
+    """
+    Asks player if they want to be "X" or "O" and
+    Computer becomes the other one
+
+
+    """
+
+
+def play_game():
+    """
+    
+    """
+
+def player_positions():
+    """
+    
+    """
+
+def check_winner():
+    """
+    
+    """
+
+
+
+def main():
+    """
+    Call on all game functions, all collected inside one function for
+    cleaner code
+    """
+    game_board()
